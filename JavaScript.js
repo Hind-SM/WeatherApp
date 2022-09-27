718091cf9b0ac077a8129f35d75f81bea9b5c8c5
Hind-SM <hind.sarahmel@gmail.com>
2022-09-27 15:43:07 +0100

    added unit converter function

--- JavaScript.js
+++ JavaScript.js
@@ -1,4 +1,4 @@
-
+//variables
 let now = new Date();
 let day = now.getDate();
 let month_array = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
@@ -11,34 +11,18 @@ let currentTime = `${hours}:${minutes}`;
 let DateCard = document.querySelector("#date");
 DateCard.innerHTML = `${currentDate} <br/> ${currentTime}`;
 
-let CelsiusButton = document.querySelector("#celsius-link");
-let FahrenheitButton = document.querySelector("#fahrenheit-link");
-let Ctemp = (document.querySelector(".card-temperature")).innerHTML
-let Ftemp = Math.round((Ctemp*(9/5))+32)
-let TempCard = document.querySelector(".card-temperature")
 
-function Celsius_to_Fahrenheit(event) {
-    event.preventDefault();
-    TempCard.innerHTML = Ctemp
-};
-
-function Fahrenheit_to_Celsius(event) {
-    event.preventDefault
-    TempCard.innerHTML = Ftemp
-};
-
-CelsiusButton.addEventListener("click", Celsius_to_Fahrenheit);
-FahrenheitButton.addEventListener("click", Fahrenheit_to_Celsius);
-
+//getting weather from api
 function showWeather(response) {
-    let currentCity = document.querySelector(".City_Name");
+    let currentCity = document.querySelector(".City-Name");
     let temperature = Math.round(response.data.main.temp);
     let cityName = response.data.name
+    CelsiusTemp = temperature;
     currentCity.innerHTML = `${cityName}`;
     TempCard.innerHTML = `${temperature}`;
     console.log(response.data);
 }
-
+//ask user from current position 
 function retrievePosition(position) {
     let apiKey = "4ee75d8fd0a41a9e7dfc26c980f6da70";
     let lat = position.coords.latitude;
@@ -54,10 +38,11 @@ function geolocation() {
 let Locatorbutton = document.querySelector(".LocatorButton");
 Locatorbutton.addEventListener("click",geolocation);
 
+//function that takes user input, then calls showweather function
 function searchCity(event) {
     event.preventDefault();
     let input = document.querySelector("#city-input");
-    let output = document.querySelector(".City_Name");
+    let output = document.querySelector(".City-Name");
     output.innerHTML = input.value;
     let city = input.value;
     let apiKey = "4ee75d8fd0a41a9e7dfc26c980f6da70";
@@ -66,3 +51,25 @@ function searchCity(event) {
 };
 let SearchInput = document.querySelector("form");
 SearchInput.addEventListener("submit", searchCity);
+
+//unit converter functions
+function Display_CelsiusTemp(event) {
+    event.preventDefault();
+    TempCard.innerHTML = CelsiusTemp;
+};
+
+function Display_FarenheitTemp(event) {
+    event.preventDefault();
+    console.log(CelsiusTemp);
+    let Ftemp = (CelsiusTemp * 9) / 5 + 32;
+    TempCard.innerHTML = Math.round(Ftemp);
+};
+
+let CelsiusTemp = null;
+let CelsiusButton = document.querySelector("#celsius-link");
+let FahrenheitButton = document.querySelector("#fahrenheit-link");
+let TempCard = document.querySelector(".card-temperature")
+
+CelsiusButton.addEventListener("click", Display_CelsiusTemp);
+FahrenheitButton.addEventListener("click", Display_FarenheitTemp);
+
