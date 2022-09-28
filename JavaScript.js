aa48cac161a7ded2fba6c7aeb486846bc98d0109
Hind-SM <hind.sarahmel@gmail.com>
2022-09-28 11:17:35 +0100

    added daily forecast function

--- JavaScript.js
+++ JavaScript.js
@@ -1,5 +1,6 @@
 //variables
 let now = new Date();
+let weekday = now.getDay();
 let day = now.getDate();
 let month_array = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 let month = now.getMonth();
@@ -9,8 +10,43 @@ let minutes = now.getMinutes();
 let currentDate = `${day} ${month_array[month]} ${year}`;
 let currentTime = `${hours}:${minutes}`;
 let DateCard = document.querySelector("#date");
-DateCard.innerHTML = `${currentDate} <br/> ${currentTime}`;
+let days_array = ["Sun","Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+let today = days_array[weekday]
+DateCard.innerHTML = `${today} ${currentDate} <br/> ${currentTime}`;
 
+
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let weekdays = ["Sun","Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    return weekdays[day];
+    
+}
+
+function show_WeatherForecast(response) {
+    let forecast = (response.data.daily.slice(1,5));
+    console.log(forecast);
+    let forecastBlocks = document.querySelector(".forecast-row");
+    forecastBlocks.innerHTML = "";   
+    forecast.forEach(function (forecastday) {
+        forecastBlocks.innerHTML = forecastBlocks.innerHTML +
+            `<div class="col">
+                <div class="card-body shadow p-2 mb-5 bg-body ">
+                    <h5 class="card-title card1-day" style="margin-left:5%;">${formatDay(forecastday.dt)}</h5>
+                    <p class="card-text forecast-temp" style="margin-left:5%;">${Math.round(forecastday.temp.max)}° <span> ${Math.round(forecastday.temp.min)}°</span></p>
+                </div>`;
+    })
+
+}
+
+//daily forecast function
+function Daily_forecast(coordinates) {
+    let lon = coordinates.lon;
+    let lat = coordinates.lat;
+    let apiKey = "4ee75d8fd0a41a9e7dfc26c980f6da70";
+    let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(show_WeatherForecast);
+};
 
 //getting weather and icon and changing background with api response
 function showWeather(response) {
@@ -33,7 +69,7 @@ function showWeather(response) {
     currentCity.innerHTML = `${cityName}`;
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${iconID}@2x.png`);
 
-    if (temperature <= 10) {
+    if (temperature < 10) {
         background.style.background = "linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)";
         
     } if(temperature <= 15) {
@@ -44,8 +80,13 @@ function showWeather(response) {
         
     } else {
         background.style.background = "linear-gradient(135deg, #feec9f 15%, #fe813e 100%)";
-    }
+    };
+
+    Daily_forecast(response.data.coord);
 };
+
+
+
 //ask user from current position 
 function retrievePosition(position) {
     let apiKey = "4ee75d8fd0a41a9e7dfc26c980f6da70";
@@ -62,6 +103,8 @@ function geolocation() {
 let Locatorbutton = document.querySelector(".LocatorButton");
 Locatorbutton.addEventListener("click",geolocation);
 
+
+
 //function that takes user input, then calls showweather function
 function searchCity(event) {
     event.preventDefault();
@@ -76,10 +119,13 @@ function searchCity(event) {
 let SearchInput = document.querySelector("form");
 SearchInput.addEventListener("submit", searchCity);
 
+
+
 //unit converter functions
 function Display_CelsiusTemp(event) {
     event.preventDefault();
     TempCard.innerHTML = CelsiusTemp;
+
 };
 
 function Display_FarenheitTemp(event) {
@@ -97,13 +143,4 @@ let TempCard = document.querySelector(".card-temperature")
 CelsiusButton.addEventListener("click", Display_CelsiusTemp);
 FahrenheitButton.addEventListener("click", Display_FarenheitTemp);
 
-//icon changer function//
-// function Display_icon() {
-//     let apiKey = "4ee75d8fd0a41a9e7dfc26c980f6da70";
-//     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-//     axios.get(url).then(showWeather);
-//     icon.innerHTML = 
 
-// }
-
-let icon = document.querySelector(".Icon")
